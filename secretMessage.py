a09791d61f51457ae307d5b0be8d3976963da56b
Khushali Desai <khushali.desai@sjsu.edu>
2015-07-21 16:02:53 +0530

    secretMessage working

--- secretMessage.py
+++ secretMessage.py
@@ -1,6 +1,23 @@
 import os
+#get file names from a folder
+#os.getcwd() gets current working dir
+#loop through each files but first change dir
 def rename_file():
-	file_list=os.listdir(r"home\coding\")
-	for fl in file_list:
-	os.rename()
+    file_list = os.listdir(r"/Users/khushali/Coding/Udacity_Programming_Foundation_with_Python/prank")
+    saved_path = os.getcwd()
+    os.chdir(r"/Users/khushali/Coding/Udacity_Programming_Foundation_with_Python/prank")
+	
+    for file_name in file_list:
+        os.rename(file_name,file_name.translate(None,"0123456789"))#old,new
+        #file_name.translate(None,"012334")(table which translates one char to another set of char,list of char to remove)
+    os.chdir(saved_path)
+             
+rename_file()
+
+#python -m tabnanny secretMessage.py
+#for indentation error
+
+
+
+
 	
