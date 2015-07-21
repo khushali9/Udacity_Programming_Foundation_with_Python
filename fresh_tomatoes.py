287a80da71b2a9ddf482e685de5b1dd283043e68
Khushali Desai <khushali.desai@sjsu.edu>
2015-07-22 00:01:48 +0530

    movie trailer working

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -128,17 +128,19 @@ movie_tile_content = '''
 def create_movie_tiles_content(movies):
     # The HTML content for this section of the page
     content = ''
-    for movie in movies:
+    
+    for movie1 in movies:
         # Extract the youtube ID from the url
-        youtube_id_match = re.search(r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
-        youtube_id_match = youtube_id_match or re.search(r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
-        trailer_youtube_id = youtube_id_match.group(0) if youtube_id_match else None
+        
+        #youtube_id_match = re.search(r'(?<=v=)[^&#]+', movie1.trailer_youtube_url)
+        #youtube_id_match = youtube_id_match or re.search(r'(?<=be/)[^&#]+', movie1.trailer_youtube_url)
+        #trailer_youtube_id = youtube_id_match.group(0) if youtube_id_match else None
 
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
-            movie_title=movie.title,
-            poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
+            movie_title=movie1.title,
+            poster_image_url=movie1.poster_image_url,
+            trailer_youtube_url=trailer_youtube_url
         )
     return content
 
--- media.py
+++ media.py
@@ -1,10 +1,10 @@
 import webbrowser
 class Movie():
-	def __init__(self,movie_t,st,pos,trailer):
-		self.title=movie_t
-		self.storyline=st
-		self.poster_image_url=pos
-		self.trailer_youtube_url=trailer
+	def __init__(self,movie_title,movie_storyline,poster_image_url,trailer_youtube_url):
+		self.title=movie_title
+		self.storyline=movie_storyline
+		self.poster_image_url=poster_image_url
+		self.trailer_youtube_url=trailer_youtube_url
 	#init for intializing the space n remember its  var n fun
 	#__XX__ means XX is reserved work in python
 	def show_trailer(self):
