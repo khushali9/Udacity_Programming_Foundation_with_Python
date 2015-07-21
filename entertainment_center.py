fe59d82cf007e88b75ecc509c72ecc2419e74852
Khushali Desai <khushali.desai@sjsu.edu>
2015-07-21 23:28:05 +0530

    error in naming

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,5 +1,5 @@
 import media
-import frsh_tomatoes
+import fresh_tomatoes
 
 toy_story=media.Movie("Toy Story","A story of a boy and his toys that come to life","https://en.wikipedia.org/wiki/Toy_Story#/media/File:Toy_Story.jpg","https://www.youtube.com/watch?v=KYz2wyBy3kc")
 bahubali=media.Movie("Baahubali: The Beginning","A dispute between brothers spans across two generations","https://en.wikipedia.org/wiki/Baahubali:_The_Beginning#/media/File:Baahubali_poster.jpg","https://www.youtube.com/watch?v=sOEg_YZQsTI")
