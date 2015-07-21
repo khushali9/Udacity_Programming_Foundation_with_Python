163310c70c79e63e61f2b55ddc6f16c496c3b7c1
Khushali Desai <khushali.desai@sjsu.edu>
2015-07-21 18:10:38 +0530

    Movie Website

---
+++ entertainment_center.py
@@ -0,0 +1,4 @@
+import media
+
+toy_story=media.Movie("Toy Story","A story of a boy and his toys that come to life","https://en.wikipedia.org/wiki/Toy_Story#/media/File:Toy_Story.jpg","https://www.youtube.com/watch?v=KYz2wyBy3kc")
+print(toy_story.storyline)
---
+++ media.py
@@ -0,0 +1,10 @@
+class Movie():
+	def_init_(self,movie_t,st,pos,trailer):
+		self.title=movie_t
+		self.storyline=st
+		self.poster_image_url=pos
+		self.trailer_youtube_url=trailer
+	#init for intializing the space n remember its  var n fun
+	#__XX__ means XX is reserved work in python
+	def show_trailer(self):
+		webbrowser.open(self.trailer_youtube_url)
