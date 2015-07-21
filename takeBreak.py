088e3db9c80b26792299798c4c56305353bccf7d
Khushali Desai <khushali.desai@sjsu.edu>
2015-07-21 15:07:19 +0530

    edited take break project

--- takeBreak.py
+++ takeBreak.py
@@ -1,5 +1,11 @@
 #project to open browser and play video
 import webbrowser
 import time
-time.sleep(10)#sec
-webbrowser.open("https://www.youtube.com/watch?v=NobzfIebbrE")
+
+total_breaks=4
+break_count=0
+print("This Program started on"+time.ctime())
+while(break_count<total_breaks):
+    time.sleep(10)#sec
+    webbrowser.open("https://www.youtube.com/watch?v=NobzfIebbrE")
+    break_count=break_count+1
