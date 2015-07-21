035f14f25c5d87cce533a0e7c34fce4f89ee56ea
Khushali Desai <khushali.desai@sjsu.edu>
2015-07-21 12:42:20 +0530

    created files

---
+++ open_browser.py
@@ -0,0 +1,5 @@
+#project to open browser and play video
+import webbrowser
+import time
+time.sleep(10)#sec
+webbrowser.open("https://www.youtube.com/watch?v=NobzfIebbrE")
---
+++ rename_file.py
@@ -0,0 +1,6 @@
+import os
+def rename_file():
+	file_list=os.listdir(r"home\coding\")
+	for fl in file_list:
+	os.rename()
+	
